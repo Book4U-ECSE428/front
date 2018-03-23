83025bc1758cd9dc2ec314d128d145d19aac09d8
Pby <pby@localhost.localdomain>
2018-03-22 23:15:05 -0400

    S13T1 and S13T2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
+var category=false;
+var cardList;
+
 $.getScript('/js/serverIP.js', function()
 {
     function logout() {
         setCookie("", "", 10);
         window.location = 'login.html';
     }
-
+    
     function setCookie(cname, cvalue, sec) {
         var d = new Date();
         d.setTime(d.getTime() + (sec * 1000));
         var expires = "expires=" + d.toUTCString();
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-        console.log("cookie", document.cookie)
+        //console.log("cookie", document.cookie);
+
     }
+
 
     $(function () {
         $(".loggedin-div").load("nav.html");
+        // Toggle category mode on click
+        $("#byCategory").click(function(e){
+            console.log(category);
+            category = !category;
+            Vue.set(cardList, 'cat', category);
+        });
     });
 
     function getCookie(cname) {
@@ -59,10 +70,44 @@ $.getScript('/js/serverIP.js', function()
     }
 
     function generateAllCard(book_list) {
-        var cardList = new Vue({
+        cardList = new Vue({
             el: "#card-list",
             data: {
-                books: book_list
+                books: book_list,
+                cat: category,
+                cat_list: [],
+                show_cat: -1
+            },
+            methods:{
+                fillCategoryList(){
+                    for (var i=0; i<this.books.length;i++){
+                        var book_cat = this.books[i]['book_category'];
+                        for (var j=0; j<book_cat.length; j++){
+                            if (this.cat_list.indexOf(book_cat[j])==-1){
+                                // console.log(book_cat[j]);
+                                this.cat_list.push(book_cat[j]);
+                            }
+                        }
+                    }
+                },
+                catCheck(cat_index, book_index){
+                    if (this.show_cat>=0 && this.show_cat!=cat_index){
+                        return false;
+                    }
+                    if (this.books[book_index]['book_category'].indexOf(this.cat_list[cat_index])>-1){
+                        return true;
+                    } else {
+                        return false;
+                    }
+
+                },
+                showCategory(cat_index){
+                    this.show_cat=cat_index;
+                    $(".heightlim").css({'max-height': 'none'});
+                }
+            },
+            created(){
+                this.fillCategoryList();
             }
         })
     }
